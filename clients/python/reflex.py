1460e6472ec52c15b0e9d9a9024e670b85ff0ffa
Garret Heaton <powdahound@gmail.com>
2009-06-14 16:46:04 -0700

    These were actually for a previous commit, but I forgot to git add :(

---
+++ clients/python/reflex.py
@@ -0,0 +1,27 @@
+import socket
+
+# Stat types
+TYPE_SUM = 1
+TYPE_AVERAGE = 2
+
+class ReflexClient:
+  def __init__(self, host, port=7828):
+    self.host = host
+    self.port = port
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+  def send(self, type, key, value):
+    msg = "%d %s %d" % (type, key, value)
+    self.sock.sendto(msg, (self.host, self.port))
+
+  def sendSum(self, key, value=1):
+    self.send(TYPE_SUM, key, value)
+
+  def sendAverage(self, key, value):
+    self.send(TYPE_AVERAGE, key, value)
+
+if __name__ == "__main__":
+  print "Sending test_stat to 127.0.0.1"
+  client = ReflexClient('127.0.0.1')
+  client.sendSum('test_sum')
+  client.sendAverage('test_average', 5)
